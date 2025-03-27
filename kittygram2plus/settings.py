860a9ef1e5c424233de74303f7beb8937dafa69e
Raman Bachyla <raman.bachyla@gmail.com>
2025-03-27 19:03:53 -0400

    init permissions

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -128,11 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
