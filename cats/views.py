614432db235deae7e31f872d7e50d1b7be4e7676
Raman Bachyla <raman.bachyla@gmail.com>
2025-03-27 21:05:02 -0400

    added pagination and throttling

--- cats/views.py
+++ cats/views.py
@@ -1,25 +1,27 @@
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle
 
 from .models import Achievement, Cat, User
-
-from .permissions import OwnerOrReadOnly
+from .pagination import CatsPagination
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    pagination_class = CatsPagination
     permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (AnonRateThrottle, WorkingHoursRateThrottle)
+    throttle_scope = 'low_request'
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
     def get_permissions(self):
-    # Если в GET-запросе требуется получить информацию об объекте
         if self.action == 'retrieve':
-            # Вернём обновлённый перечень используемых пермишенов
             return (ReadOnly(),)
-        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
         return super().get_permissions()
 
 
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -134,6 +134,18 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        # 'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10000/day',
+        'anon': '1000/day',
+        'low_request': '1/minute',
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 SIMPLE_JWT = {
